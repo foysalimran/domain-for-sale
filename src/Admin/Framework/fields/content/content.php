d11d99927b4778dd6363baa467b2ee3175133bf0
sakhawat9 <sakhawathossain7969@gmail.com>
2024-08-06 15:33:55 +0600

    update admin code

---
+++ src/Admin/Framework/fields/content/content.php
@@ -0,0 +1,28 @@
+<?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access directly.
+/**
+ *
+ * Field: content
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ *
+ */
+if ( ! class_exists( 'DOMAIN_FOR_SALE_Field_content' ) ) {
+  class DOMAIN_FOR_SALE_Field_content extends DOMAIN_FOR_SALE_Fields {
+
+    public function __construct( $field, $value = '', $unique = '', $where = '', $parent = '' ) {
+      parent::__construct( $field, $value, $unique, $where, $parent );
+    }
+
+    public function render() {
+
+      if ( ! empty( $this->field['content'] ) ) {
+
+        echo $this->field['content'];
+
+      }
+
+    }
+
+  }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'themeatelier/domain-for-sale',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '100f866c3e431ec13acbf64ef6ea0db18fb8ae06',
+        'type' => 'wordpress-plugins',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'themeatelier/domain-for-sale' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '100f866c3e431ec13acbf64ef6ea0db18fb8ae06',
+            'type' => 'wordpress-plugins',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
